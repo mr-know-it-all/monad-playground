f196672d817129add8d1fafeef8c46530d849b26
Paul <paul.constantin.chitic@gmail.com>
2019-05-30 23:08:05 +0300

    tests update

--- tests/either-test.js
+++ tests/either-test.js
@@ -1,49 +1,49 @@
 const { right, left, Either } = require('../src/either.js');
 const assert = require('assert');
 
-const getUserId = ({ id = null}) => id;
-const getUserName = ({ name = null }) => name;
-const getUserAge  = ({ age = null }) => age;
-
-const registeredUserIds = [1, 2, 3, 4];
-const isUserIdRegistered = registry => id => registry.includes(id);
-const validUserNames = ['a', 'b', 'c', 'd'];
-const isUserNameValid = names => name => names.includes(name);
-const isValidAge = age => age > 30;
-
-const validateId = user => (
-  isUserIdRegistered(registeredUserIds)(getUserId(user))
-    ? right(user)
-    : left(['invalid user id'])
-);
-
-const validateName = user => (
-  isUserNameValid(validUserNames)(getUserName(user))
-    ? right(user)
-    : left(['invalid user name'])
-);
-
-const validateAge = user => (
-  isValidAge(getUserAge(user))
-    ? right(user)
-    : left(['invalid user age'])
-);
-
-const validateUser = user => right(user).bind(validateId).bind(validateName).bind(validateAge);
-
-const invalidUserId = validateUser({ id: 100, name: 'a', age: 42 });
-const invalidUserName = validateUser({ id: 1, name: 'x', age: 42 });
-const invalidUserAge = validateUser({ id: 1, name: 'a', age: 0 });
-const validUser = validateUser({ id: 1, name: 'a', age: 42 });
-
-const onError = err => ['error', err];
-const onSuccess = res => ['success', res];
-
-const doAction = e => Either(onError, onSuccess, e);
-
 xdescribe('Either Monad laws', () => {})
 
 describe('Either Monad test', () => {
+  const getUserId = ({ id = null}) => id;
+  const getUserName = ({ name = null }) => name;
+  const getUserAge  = ({ age = null }) => age;
+
+  const registeredUserIds = [1, 2, 3, 4];
+  const isUserIdRegistered = registry => id => registry.includes(id);
+  const validUserNames = ['a', 'b', 'c', 'd'];
+  const isUserNameValid = names => name => names.includes(name);
+  const isValidAge = age => age > 30;
+
+  const validateId = user => (
+    isUserIdRegistered(registeredUserIds)(getUserId(user))
+      ? right(user)
+      : left(['invalid user id'])
+  );
+
+  const validateName = user => (
+    isUserNameValid(validUserNames)(getUserName(user))
+      ? right(user)
+      : left(['invalid user name'])
+  );
+
+  const validateAge = user => (
+    isValidAge(getUserAge(user))
+      ? right(user)
+      : left(['invalid user age'])
+  );
+
+  const validateUser = user => right(user).bind(validateId).bind(validateName).bind(validateAge);
+
+  const invalidUserId = validateUser({ id: 100, name: 'a', age: 42 });
+  const invalidUserName = validateUser({ id: 1, name: 'x', age: 42 });
+  const invalidUserAge = validateUser({ id: 1, name: 'a', age: 0 });
+  const validUser = validateUser({ id: 1, name: 'a', age: 42 });
+
+  const onError = err => ['error', err];
+  const onSuccess = res => ['success', res];
+
+  const doAction = e => Either(onError, onSuccess, e);
+
   it('test user with invalid id', () => {
     assert.deepEqual(
       ['error', ['invalid user id']],
